1646db7c864b04b95735729d377a1cb245000427
3x1 <EngFadyMondy@gmail.com>
2022-05-18 14:22:04 +0200

    v2

--- Modules/Base/Helpers/Menu/List.php
+++ Modules/Base/Helpers/Menu/List.php
@@ -18,11 +18,3 @@ app()->setLocale($local);
 Core::registerDashboardMenuItem(Menu::make('Dashboard')->icon('bx bxs-home')->route('dashboard'));
 Core::registerProfileMenuItem(Menu::make('Profile')->icon('bxs-user')->route('profile.show'));
 Core::registerProfileMenuItem(Menu::make('API Tokens')->icon('bxs-check-shield')->route('api-tokens.index'));
-
-Core::registerDashboardMenuItem(Menu::make('Group')->icon('bx bxs-circle')->route('groups.index')->can('view_any_groups'));
-
-Core::registerDashboardMenuItem(Menu::make('Customer')->icon('bx bxs-circle')->route('customers.index')->can('view_any_customers'));
-
-Core::registerDashboardMenuItem(Menu::make('CustomersLocation')->icon('bx bxs-circle')->route('customers_locations.index')->can('view_any_customers_locations'));
-
-Core::registerDashboardMenuItem(Menu::make('Loader')->icon('bx bxs-circle')->route('loaders.index')->can('view_any_loaders'));
