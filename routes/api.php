7ea81983546ffc3012952d76dddce03483792b56
3x1 <EngFadyMondy@gmail.com>
2022-05-25 04:22:28 +0200

    v3

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::resource('loaders', App\Http\Controllers\API\LoaderAPIController::class);
-
-
-Route::resource('groups', App\Http\Controllers\API\GroupAPIController::class);
-
-
-Route::resource('customers', App\Http\Controllers\API\CustomerAPIController::class);
-
-
-Route::resource('customers_locations', App\Http\Controllers\API\CustomersLocationAPIController::class);
